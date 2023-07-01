052c5ef8a496046f88ee592a5c02b81159fb3b81
Masoom-Wahid <masoom3892@gmail.com>
2023-07-01 17:32:02 +0430

    Changing Some Lines

--- todo/base/admin.py
+++ todo/base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tasks,Date
+from .models import Tasks,Date,DateActivity
 
 admin.site.register(Tasks)
 admin.site.register(Date)
+admin.site.register(DateActivity)
